b0105367a34f5359a68d7ba7cf817a81b8205159
Koosel <Koosel@protonmail.com>
2022-04-24 04:53:09 -0300

    feat: regra and show Moon Phases Result

--- codeExecute.py
+++ codeExecute.py
@@ -1,12 +1,15 @@
+# %%
 import technicalAnalysis  as ta
 import binanceAPI as bAPIfunc
 import json
 from datetime import *
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from skyfield.api import load
 from skyfield.framelib import ecliptic_frame
 
+# %%
 # Generate keys or load them
 try:
     with open('keys.json', 'r') as f:
@@ -26,21 +29,17 @@ SECRET_KEY  = keys[1]
 
 spot_bapi = bAPIfunc.spot_Kline(API_KEY,SECRET_KEY)
 
-# candles = spot_bapi.get_AllKlines("1D", 86400000,)
-# candlesList = ta.attr_loop(candles)
-# candleValue = pd.DataFrame(candlesList)
-# candleValue.to_csv('spot_dailyCandles', encoding='utf-8', index=False)
-
 spot_120m_CandleValues  = pd.read_csv('spot_120m_Candles.csv')
 spot_Daily_CandleValues = pd.read_csv('spot_dailyCandles.csv')
 
 date = ta.Date()
+Astro   = ta.Astronomy_Skyfield()
 dateTime = []
 
+# %%
 for x in spot_Daily_CandleValues['TimeOpen']:
     dateTime.append(date.get_date(x))
 
-Astro   = ta.Astronomy_Skyfield()
 
 moonType = []
 
@@ -77,32 +76,94 @@ for x in moonPhases:
 
 df_MoonPhases = pd.DataFrame()
 df_MoonPhases['MoonPhase'] = moonPhasesCycle
-# moonType = []
+df_MoonPhases['Ativo'] = spot_Daily_CandleValues['Close']
 
-# for x in spot_Daily_CandleValues['Datetime']:
-#     moon = MP.setDate(x).getMoonPhase()
-#     moonType.append(moon)
+# %%
+def regra(base):
+    preco_compra = []
+    preco_venda = []
+    aux = 0
+    for i in range(len(base)):
+        if base['MoonPhase'][i] == 'Lua Nova':
+            if aux != 1:
+                preco_compra.append(base['Ativo'][i])
+                preco_venda.append(np.nan)
+                aux = 1
+            else:
+                preco_compra.append(np.nan)
+                preco_venda.append(np.nan)
+        elif base['MoonPhase'][i] == 'Lua Cheia':
+            if aux != 0:
+                preco_compra.append(np.nan)
+                preco_venda.append(base['Ativo'][i])
+                aux = 0
+            else:
+                preco_compra.append(np.nan)
+                preco_venda.append(np.nan)
+        else:
+            preco_compra.append(np.nan)
+            preco_venda.append(np.nan)
+    
+    return (preco_compra, preco_venda)
 
-# spot_Daily_CandleValues['MoonPhase'] = moonType
+compra,venda = regra(df_MoonPhases)
 
-# df_MoonPhase = spot_Daily_CandleValues['MoonPhase']
+sinal = pd.DataFrame()
+sinal["Compra"] = compra
+sinal["Venda"] = venda
+df_MoonPhases["Compra"] = compra
+df_MoonPhases["Venda"] = venda
 
-# fullMoon = []
+df_regra = df_MoonPhases
 
-# for x in df_MoonPhase:
-#     if x == 'Lua Cheia':
-#         fullMoon.append(x)
-#     else:
-#         fullMoon.append(np.nan)
+# %%
+plt.figure(figsize = (16, 10))
+plt.plot(df_regra["Ativo"], label = "Ativo", alpha = 0.75, linewidth = 2, color = "blue")
+plt.scatter(df_regra.index, df_regra["Compra"]*0.95, label = "Compra", marker = "^", s = 100, color = "green")
+plt.scatter(df_regra.index, df_regra["Venda"]*1.05, label = "Venda", marker = "v", s = 100, color = "red")
+plt.title("Regra de trade - Cruzamento de Médias")
+plt.legend(loc = "lower right")
+plt.xlabel("2017 à 2022")
+plt.ylabel("Cotação")
+plt.show()
 
-# newMoon = []
+# %%
+df_compra = df_regra[ ~ df_regra["Compra"].isna()]
+np.array(df_compra["Compra"])
+df_venda = df_regra[ ~ df_regra["Venda"].isna()]
+np.array(df_venda["Venda"])
 
-# for x in df_MoonPhase:
-#     if x == 'Lua Nova':
-#         newMoon.append(x)
-#     else:
-#         newMoon.append(np.nan)
+Compra  = df_regra['Compra'].dropna(axis=0).values.tolist()
+Venda   = df_regra['Venda'].dropna(axis=0).values.tolist()
 
-# df_MoonPhases = pd.DataFrame()
-# df_MoonPhases['fullMoon']   = fullMoon
-# df_MoonPhases['newMoon']    = newMoon
+
+df = pd.DataFrame()
+df['Compra']    = Compra
+df['Venda']     = Venda
+
+# %%
+Trade = []
+for x in df:
+    Trade.append(df['Venda'] - df['Compra'])
+
+df['Trade'] = Trade[0]
+
+resultado = [0]
+index = -1
+for x in df['Trade']:
+    index += 1
+    resultado.append(resultado[index] + x)
+
+df['Resultado'] = resultado[-(len(resultado)-1):]
+
+# %%
+
+plt.figure(figsize = (16, 10))
+plt.plot(df["Resultado"], label = "Ativo", alpha = 0.75, linewidth = 2, color = "blue")
+plt.hlines(0,0,max(df.index.values))
+plt.vlines(df['Resultado'].idxmax(),0,df['Resultado'].max())
+plt.title("Regra de trade - Fases da Lua")
+plt.legend(loc = "lower right")
+plt.xlabel("2017 à 2022")
+plt.ylabel("Cotação")
+plt.show()
