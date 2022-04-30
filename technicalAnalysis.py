f565245acb383fce92d4fc6116b17d40b23c7bc6
Koosel <Koosel@protonmail.com>
2022-04-30 13:45:37 -0300

    feat: get Moon Degree, Moon Phases and Moon 2 Phases

--- technicalAnalysis.py
+++ technicalAnalysis.py
@@ -121,7 +121,7 @@ class Astronomy_Skyfield:
 
     def getPlanetPhase(self, lat = 42.21 * N, long= -71.03 * W, planet='mars'): 
         ts = load.timescale()
-        t = ts.utc(*self.dateParams)
+        t = ts.utc(*self.referenceDatetime)
 
         eph = load('de421.bsp')
         sun, Planet, earth = eph['sun'], eph[planet], eph['earth']
@@ -133,6 +133,98 @@ class Astronomy_Skyfield:
         phase = (m.degrees - s.degrees) % 360.0
 
         return phase
+
+# Refatorar o código
+    def getMoon_Degree(self,yf_Dataframe): #get moon degree based on Dataframe index
+        dateTime = []
+
+        for x in yf_Dataframe.index:
+            dateTime.append(x)
+
+        PT1 = int((len(dateTime)-1) * 0.25)
+        PT2 = int((len(dateTime)-1) * 0.5)
+        PT3 = int((len(dateTime)-1) * 0.75)
+        PT4 = int((len(dateTime)-1) * 1)
+
+        X = dateTime[:PT1]
+        Y = dateTime[PT1:PT2]
+        Z = dateTime[PT2:PT3]
+        W = dateTime[PT3:]
+
+        moonType = []
+        start = time.time()
+
+        print("Loop de Atribuição iniciado")
+        for x in X:
+            moon = self.setDatetime(x).getMoonPhase()
+            moonType.append(moon)
+
+        PT1 = time.time()
+        print('25% Concluido')
+        print(len(moonType))
+        print(str(PT1 - start)+'\n')
+
+        for x in Y:
+            moon = self.setDatetime(x).getMoonPhase()
+            moonType.append(moon)
+
+        PT2 = time.time()
+        print('50% Concluido')
+        print(len(moonType))
+        print(str(PT2 - PT1)+'\n')
+
+        for x in Z:
+            moon = self.setDatetime(x).getMoonPhase()
+            moonType.append(moon)
+
+        PT3 = time.time()
+        print('75% Concluido')
+        print(len(moonType))
+        print(str(PT3 - PT2)+'\n')
+
+        for x in W:
+            moon = self.setDatetime(x).getMoonPhase()
+            moonType.append(moon)
+
+        PT4 = time.time()
+        print('100% Concluido')
+        print(len(moonType))
+        print(str(PT4 - PT3)+'\n')
+
+        print("Duration: "+str(PT4 - start))
+        return moonType
+
+    def getMoon_Phases(self,moon_Degree):
+        moonPhases = []
+
+        for x in moon_Degree:
+            if x < 90:
+                moonPhases.append('Lua Nova')
+            if x >= 90 and x < 180:
+                moonPhases.append('Lua Crescente')
+            if x >= 180 and x < 270:
+                moonPhases.append('Lua Cheia')
+            if x >= 270 and x < 360:
+                moonPhases.append('Lua Minguante')
+            if x >= 360:
+                moonPhases.append('Lua Nova')
+
+        return moonPhases
+
+# %%
+
+    def getMoon_Phases_2_Phases(self,moon_Degree):
+        moonPhases = []
+
+        for x in moon_Degree:
+            if x < 180:
+                moonPhases.append('Lua Nova')
+            if x >= 180 and x < 360:
+                moonPhases.append('Lua Cheia')
+            if x >= 360:
+                moonPhases.append('Lua Nova')
+
+        return moonPhases
 
 class Attribute:
     def attr_loop(klines):
