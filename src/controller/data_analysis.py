6e648af050f049463f3390973144f487936c5316
Koosel <Koosel@protonmail.com>
2022-05-05 15:08:55 -0300

    refactor: add total_days param in correlation method

--- src/controller/data_analysis.py
+++ src/controller/data_analysis.py
@@ -12,11 +12,11 @@ class LinearRegressionModels:
         r_sq = model.score(xArray, yArray)
         return r_sq
 
-    def correlation(self, firstAsset_DataFrame, secondAsset_DataFrame):
+    def correlation(self, firstAsset_DataFrame, secondAsset_DataFrame,total_days=1000):
 
         first_DataFrame = pd.DataFrame()
         first_DataFrame["Close"] = firstAsset_DataFrame["Close"]
-        first_DataFrame = first_DataFrame[-1000:]
+        first_DataFrame = first_DataFrame[-total_days:]
         first_DataFrame["Lagged Close"] = first_DataFrame["Close"].shift(-1)
         first_DataFrame["LN"] = np.log(
             first_DataFrame["Close"] / first_DataFrame["Lagged Close"]
@@ -25,7 +25,7 @@ class LinearRegressionModels:
 
         second_DataFrame = pd.DataFrame()
         second_DataFrame["Close"] = secondAsset_DataFrame["Close"]
-        second_DataFrame = second_DataFrame[-1000:]
+        second_DataFrame = second_DataFrame[-total_days:]
         second_DataFrame["Lagged Close"] = second_DataFrame["Close"].shift(-1)
         second_DataFrame["LN"] = np.log(
             second_DataFrame["Close"] / second_DataFrame["Lagged Close"]
